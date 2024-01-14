e17920ab453602eb8886085302ab01dfa9dd0b09
Melisa Vlad <melisa.vlad@s.unibuc.ro>
2024-01-14 20:38:05 +0200

    Finish register

--- src/main/java/com/univbuc/bookreview/services/UserService.java
+++ src/main/java/com/univbuc/bookreview/services/UserService.java
@@ -1,5 +1,7 @@
 package com.univbuc.bookreview.services;
 
+import com.univbuc.bookreview.models.Role;
+import com.univbuc.bookreview.repositories.RoleRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -19,8 +21,13 @@ public class UserService {
     @Autowired
     private JwtUtil jwtUtil;
 
+    @Autowired
+    private RoleRepository roleRepository;
+
     public User registerUser(User user) {
+        Role userRole = roleRepository.findByRoleName("USER");
         user.setPassword(passwordEncoder.encode(user.getPassword()));
+        user.setRole(userRole);
         return userRepository.save(user);
     }
 
